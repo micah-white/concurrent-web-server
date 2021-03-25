a86222494ac7737b8fad5056e3591d3360b944a3
micah-white <mcwhite9@crimson.ua.edu>
2021-03-24 23:48:24 -0500

    bed time

--- wserver.c
+++ wserver.c
@@ -5,7 +5,7 @@
 char default_root[] = ".";
 
 //
-// ./wserver [-d <basedir>] [-p <portnum>] 
+// ./wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]
 // 
 int main(int argc, char *argv[]) {
     int c;
@@ -17,37 +17,37 @@ int main(int argc, char *argv[]) {
     
     while ((c = getopt(argc, argv, "d:p:t:b:s:")) != -1)
 	switch (c) {
-	case 'd':
-	    root_dir = optarg;
-	    break;
-	case 'p':
-	    port = atoi(optarg);
-	    break;
-	case 't':
-		numThreads = atoi(optarg);
-		if(numThreads < 1){
-			fprintf(stderr, "number of threads must be a positive integer\n");
-	    	exit(1);
-		}
-		break;
-	case 'b':
-		bufferSize = atoi(optarg);
-		if(bufferSize < 1){
-			fprintf(stderr, "number of buffers must be positive integer\n");
-	    	exit(1);
-		}
-		break;
-	case 's':
-		if(strcmp("SFF", optarg) == 0)
-			schedulingAlgorithm = 1;
-		else if(strcmp("FIFO", optarg) != 0){
-			fprintf(stderr, "Scheduling algorithm must be either FIFO or SFF\n");
+		case 'd':
+			root_dir = optarg;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case 't':
+			numThreads = atoi(optarg);
+			if(numThreads < 1){
+				fprintf(stderr, "number of threads must be a positive integer\n");
+				exit(1);
+			}
+			break;
+		case 'b':
+			bufferSize = atoi(optarg);
+			if(bufferSize < 1){
+				fprintf(stderr, "number of buffers must be positive integer\n");
+				exit(1);
+			}
+			break;
+		case 's':
+			if(strcmp("SFF", optarg) == 0)
+				schedulingAlgorithm = 1;
+			else if(strcmp("FIFO", optarg) != 0){
+				fprintf(stderr, "Scheduling algorithm must be either FIFO or SFF\n");
+				exit(1);
+			}
+			break;
+		default:
+			fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]\n");
 			exit(1);
-		}
-		break;
-	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
-	    exit(1);
 	}
 
     // run out of this directory
@@ -64,3 +64,5 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+
+void thread()
